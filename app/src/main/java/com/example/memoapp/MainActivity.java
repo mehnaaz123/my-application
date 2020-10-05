bbdc3e368b745bd20222cc21ba7783a35ba164ee
mehnaaz123 <m246rahm@ryerson.ca>
2020-10-04 21:46:07 -0400

    Fifth Commit

--- app/src/main/java/com/example/memoapp/MainActivity.java
+++ app/src/main/java/com/example/memoapp/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         myMemoData = new MemoData("Important Emails", "Read emails from Cathy and Luke", "Don't forget to acknowledge the emails and tell Luke that there is a meeting tom!");
         myMemoList.add(myMemoData);
 
-        myMemoData = new MemoData("Grocery List", "Remember to buy these on your way from the dentist appointment on Thursday!", " Milk\n Bread\n Eggs\n Bagels\n Jell-O\n Noodles\n Coriander\n Oregano");
+        myMemoData = new MemoData("Grocery List", "Remember to buy these on your way home from the dentist appointment on Thursday!", " Milk\n Bread\n Eggs\n Bagels\n Jell-O\n Noodles\n Coriander\n Oregano");
         myMemoList.add(myMemoData);
 
         myMemoData = new MemoData("Textbooks To Buy", "Order Sadaf's textbooks for her classes this semester", " https://www.amazon.ca/Canadian-Criminal-Justice-Curt-Griffiths/dp/0176529209\n\n https://www.wiley.com/en-us/Big+Java%3A+Late+Objects%2C+1st+Edition-p-9781119626152#:~:text=Big%20Java%3A%20Late%20Objects%2C%201st%20Edition%20%7C%20Wiley,first%20course%20in%20programming%20for%20computer%20science%20students.");
